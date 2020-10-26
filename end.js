2d34cbef1a432a9c229ce95acf9737c3888685ac
fhan95 <felix.han.1995@gmail.com>
2020-10-25 20:30:47 -0400

    Fix naming convention

--- end.js
+++ end.js
@@ -4,7 +4,7 @@ const finalScore = document.getElementById("finalScore");
 const mostRecentScore = localStorage.getItem("mostRecentScore");
 
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
-const MAX_HIGHSCORES = 5;
+const MAX_HIGH_SCORES = 5;
 
 finalScore.innerText = mostRecentScore;
 
@@ -23,7 +23,7 @@ saveHighScore = e => {
     highScores.push(score);
 
     highScores.sort((a, b) => b.score - a.score);
-    highScores.splice(MAX_HIGHSCORES);
+    highScores.splice(MAX_HIGH_SCORES);
 
     localStorage.setItem("highScores", JSON.stringify(highScores));
     window.location.assign("./index.html");
